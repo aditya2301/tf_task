10519ac86761f4ded3b5118acdf848443b3e9812
aditya2301 <aditya_ag2301@yahoo.in>
2024-12-09 21:10:01 +0530

    Bug fix

--- env/stage/main.tf
+++ env/stage/main.tf
@@ -27,5 +27,5 @@ resource "aws_instance" "newbox-stage" {
 # Output the instance ID
 output "instance_id" {
   description = "The ID of the created EC2 instance"
-  value       = aws_instance.newbox.id
+  value       = aws_instance.newbox-stage.id
 }
